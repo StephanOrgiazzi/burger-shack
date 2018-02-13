c35e4518706ed3bbc8a953350480138d9178a16d
stephan <stephan.orgiazzi@gmail.com>
2018-02-13 20:58:51 +0100

    updated performance with shouldComponentUpdate() in Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = (props) => {
+class Modal extends Component {
 
-    return (
-        <React.Fragment>
-            <Backdrop show={props.show} click={props.modalClosed} />
-            <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? "translateY(0)" : "translateY(100vh)",
-                opacity: props.show ? "1" : "0"
-            }} >
-                {props.children}
-            </div>
-        </React.Fragment>
-    )
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render() { 
+        return (
+            <React.Fragment>
+                <Backdrop show={this.props.show} click={this.props.modalClosed} />
+                <div 
+                className={classes.Modal}
+                style={{
+                    transform: this.props.show ? "translateY(0)" : "translateY(100vh)",
+                    opacity: this.props.show ? "1" : "0"
+                }} >
+                    {this.props.children}
+                </div>
+            </React.Fragment>
+          )
+    }
 }
  
-export default modal;
+export default Modal;
