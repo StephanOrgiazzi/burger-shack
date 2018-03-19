35e0513509641baca79b92d03b56c3284a8dc1db
stephan <stephan.orgiazzi@gmail.com>
2018-03-19 20:10:20 +0100

    fixed cross browser bugs

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -6,16 +6,16 @@ const buildControl = (props) => {
     return (
         <div className={classes.BuildControl}>
             <div className={classes.Label}>{props.label}</div>
-            <button>
-                <span 
-                className={classes.Less} 
-                onClick={props.ingredientRemoved} 
-                disabled={props.disabled} ><strong>-</strong></span>
-                <span>{props.quantity}</span>
-                <span
-                className={classes.More} 
-                onClick={props.ingredientAdded} ><strong>+</strong></span>
-            </button>
+                <div className={classes.Button}>
+                    <button 
+                    className={classes.Less} 
+                    onClick={props.ingredientRemoved} 
+                    disabled={props.disabled} >-</button>
+                    <span>{props.quantity}</span>
+                    <span
+                    className={classes.More} 
+                    onClick={props.ingredientAdded} ><strong>+</strong></span>
+                </div>
         </div>
     )
 };
